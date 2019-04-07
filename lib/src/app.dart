7be16fd85fd44b1bed26418f8bd02aa6b518c01a
bensigo <sigocode@gmail.com>
2019-04-07 04:38:38 +0400

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final topStoriesIdUrl = "https://hacker-news.firebaseio.com/v0/topstories";
+final topStoriesItemUrl = "https://hacker-news.firebaseio.com/v0/item/:id";
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
